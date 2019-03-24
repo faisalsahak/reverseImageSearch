f602940cc2f94c707649a92e3ebe5ce4c32a70fa
user <user@users-MacBook-Pro.local>
2019-03-23 18:02:47 -0700

    added a button next to submit so that now we can also search for images that are similar to the image we want to search if the results are not what we are looking for

---
+++ app/src/main/java/com/example/reverseimagesearch/postrequest/SimilarImages.java
@@ -0,0 +1,18 @@
+package com.example.reverseimagesearch.postrequest;
+
+public class SimilarImages {
+
+    String image;
+
+    public SimilarImages(String image) {
+        this.image = image;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
